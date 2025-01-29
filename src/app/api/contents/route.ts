a9dbf165feb28f40eaccf49e9f30d58646c94e14
ai10pro <106215301+ai10pro@users.noreply.github.com>
2025-01-29 20:03:04 +0900

    api/contentsで動作するAPIの作成

---
+++ src/app/api/contents/route.ts
@@ -0,0 +1,20 @@
+import prisma from "@/lib/prisma";
+import { NextResponse, NextRequest } from "next/server";
+
+// [GET] /api/contents カテゴリ一覧の取得
+export const GET = async (req: NextRequest) => {
+  try {
+    const contents = await prisma.content.findMany({
+      orderBy: {
+        createdAt: "desc", // 降順 (新しい順)
+      },
+    });
+    return NextResponse.json(contents);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "カテゴリの取得に失敗しました" },
+      { status: 500 } // 500: Internal Server Error
+    );
+  }
+};
