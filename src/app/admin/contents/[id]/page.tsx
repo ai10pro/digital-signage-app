cf5c777575635397b03b441e44355c81829e5773
ai10pro <106215301+ai10pro@users.noreply.github.com>
2025-02-19 11:14:13 +0900

    update 1 file and create 1 file

--- src/app/admin/contents/[id]/page.tsx
+++ src/app/admin/contents/[id]/page.tsx
@@ -7,6 +7,11 @@ import fetchContents from "@/app/_components/FetchContents";
 import fetchUsers from "@/app/_components/FetchUsers";
 import fetchTags from "@/app/_components/FetchTags";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import { twMerge } from "tailwind-merge";
+
+// タグをフェッチしたときのレスポンスのデータ型
 type RawApiTagResponse = {
   id: string;
   name: string;
@@ -14,6 +19,7 @@ type RawApiTagResponse = {
   updatedAt: string;
 };
 
+// ユーザーをフェッチしたときのレスポンスのデータ型
 type RawApiUserResponse = {
   id: string;
   name: string;
@@ -22,40 +28,418 @@ type RawApiUserResponse = {
   updatedAt: string;
 };
 
-type RawApiContentResponse = {
+// コンテンツのタグ選択用のデータ型
+type SelectableTag = {
+  id: string;
+  name: string;
+  isSelected: boolean;
+};
+
+// コンテンツのユーザー選択用のデータ型
+type SelectableUser = {
+  id: string;
+  name: string;
+  imageURL: string;
+  isSelected: boolean;
+};
+
+type ContentApiResponse = {
   id: string;
   title: string;
   text: string;
   coverImageURL: string;
   createdAt: string;
   updatedAt: string;
-  users: RawApiUserResponse[];
-  tags: RawApiTagResponse[];
-};
-
-type SelectableTag = {
-  id: string;
-  name: string;
+  users: {
+    user: {
+      id: string;
+      name: string;
+      imageURL: string;
+    };
+  };
+  tags: {
+    tag: {
+      id: string;
+      name: string;
+    };
+  }[];
 };
 
 const Page: React.FC = () => {
   const { id } = useParams() as { id: string };
   const router = useRouter();
 
+  const [isInitialized, setIsInitialized] = useState<boolean>(false);
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
   const [fetchError, setFetchError] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [Content, setContent] = useState<RawApiContentResponse | null>(null);
+
+  const [newTitle, setNewTitle] = useState<string>("");
+  const [newText, setNewText] = useState<string>("");
+  const [newCoverImageURL, setNewCoverImageURL] = useState<string>("");
+
+  // const [content, setContent] = useState<RawApiContentResponse | null>(null);
   const [users, setUsers] = useState<RawApiUserResponse[] | null>(null);
   const [tags, setTags] = useState<SelectableTag[] | null>(null);
+  const [selectedUsers, setSelectedUsers] = useState<string[]>([]);
+  const [selectedTags, setSelectedTags] = useState<string[]>([]);
 
+  // タグ配列（State）の更新処理
+  const [checkableTags, setCheckableTags] = useState<SelectableTag[] | null>(
+    []
+  );
+  // ユーザー配列（State）の更新処理
+  const [checkableUsers, setCheckableUsers] = useState<SelectableUser[] | null>(
+    []
+  );
+  // 編集前のコンテンツ情報
+  const [content, setContent] = useState<ContentApiResponse | null>(null);
+
+  // コンテンツの取得処理
   useEffect(() => {
-    fetchContents(setContent, setFetchError, setIsLoading);
-    fetchUsers(setUsers, setFetchError, setIsLoading);
-    fetchTags(setTags, setFetchError, setIsLoading);
+    const fetchContent = async () => {
+      try {
+        const requestUrl = `/api/contents/${id}`;
+        const res = await fetch(requestUrl, {
+          method: "GET",
+          cache: "no-store",
+        });
+        if (!res.ok) {
+          setContent(null);
+          throw new Error(`${res.status}: ${res.statusText}`);
+        }
+        const apiResBody = (await res.json()) as ContentApiResponse;
+        setContent(apiResBody);
+      } catch (error) {
+        const errorMsg =
+          error instanceof Error
+            ? `投稿記事の取得に失敗しました: ${error.message}`
+            : `予期せぬエラーが発生しました ${error}`;
+        console.error(errorMsg);
+        setFetchError(errorMsg);
+      }
+    };
+    fetchContent();
+  }, [id]);
+
+  // ユーザー・タグの取得処理
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const requestUrl = "/api/users";
+        const res = await fetch(requestUrl, {
+          method: "GET",
+          cache: "no-store",
+        });
+        if (!res.ok) {
+          setCheckableUsers(null);
+          throw new Error(`${res.status}: ${res.statusText}`);
+        }
+        const apiResBody = (await res.json()) as RawApiUserResponse[];
+        setCheckableUsers(
+          apiResBody.map((user) => ({
+            id: user.id,
+            name: user.name,
+            imageURL: user.imageURL,
+            isSelected: false,
+          }))
+        );
+      } catch (error) {
+        const errorMsg =
+          error instanceof Error
+            ? `ユーザーの取得に失敗しました: ${error.message}`
+            : `予期せぬエラーが発生しました ${error}`;
+        console.error(errorMsg);
+        setFetchError(errorMsg);
+      }
+    };
+    const fetchTags = async () => {
+      try {
+        const requestUrl = "/api/tags";
+        const res = await fetch(requestUrl, {
+          method: "GET",
+          cache: "no-store",
+        });
+        if (!res.ok) {
+          setCheckableTags(null);
+          throw new Error(`${res.status}: ${res.statusText}`);
+        }
+        const apiResBody = (await res.json()) as RawApiTagResponse[];
+        setCheckableTags(
+          apiResBody.map((tag) => ({
+            id: tag.id,
+            name: tag.name,
+            isSelected: false,
+          }))
+        );
+      } catch (error) {
+        const errorMsg =
+          error instanceof Error
+            ? `タグの取得に失敗しました: ${error.message}`
+            : `予期せぬエラーが発生しました ${error}`;
+        console.error(errorMsg);
+        setFetchError(errorMsg);
+      }
+    };
+    fetchUsers();
+    fetchTags();
   }, []);
 
-  console.log(users);
-  return <></>;
-};
+  // コンテンツのデータ取得ができたら、各Stateに値をセット
+  useEffect(() => {
+    // 初期化済みなら戻る
+    if (isInitialized) return;
 
+    // コンテンツのデータが取得できていない場合は戻る
+    if (!content || !checkableTags || !checkableUsers) return;
+
+    // コンテンツのデータをStateにセット
+    setNewTitle(content.title);
+    setNewText(content.text);
+    setNewCoverImageURL(content.coverImageURL);
+
+    // タグの選択状態を更新
+    const selectedTagIds = new Set(content.tags.map((tag) => tag.tag.id));
+    setCheckableTags(
+      checkableTags.map((tag) => ({
+        ...tag,
+        isSelected: selectedTagIds.has(tag.id),
+      }))
+    );
+
+    // ユーザーの選択状態を更新
+    const selectedUserIds: Set<string> = new Set(
+      Object.values(content.users).map((c) => c.id)
+    );
+    setCheckableUsers(
+      checkableUsers.map((user) => ({
+        ...user,
+        isSelected: selectedUserIds.has(user.id),
+      }))
+    );
+
+    setIsInitialized(true);
+  }, [content, checkableTags, checkableUsers, isInitialized]);
+
+  // checkBoxの選択状態を更新する関数
+  const switchUserState = (userId: string) => {
+    if (!checkableUsers) return;
+
+    setCheckableUsers(
+      checkableUsers.map((user) =>
+        user.id === userId ? { ...user, isSelected: !user.isSelected } : user
+      )
+    );
+  };
+  const switchTagState = (tagId: string) => {
+    if (!checkableTags) return;
+
+    setCheckableTags(
+      checkableTags.map((tag) =>
+        tag.id === tagId ? { ...tag, isSelected: !tag.isSelected } : tag
+      )
+    );
+  };
+
+  const updateNewTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // ☆ タイトルのバリテーション処理
+    setNewTitle(e.target.value);
+  };
+
+  const updateNewText = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    // ☆ 本文のバリテーション処理
+    setNewText(e.target.value);
+  };
+
+  const updateNewCoverImageURL = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // ☆ カバーイメージURLのバリテーション処理
+    setNewCoverImageURL(e.target.value);
+  };
+
+  // 送信処理
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault(); // デフォルトの送信処理をキャンセル
+    setIsSubmitting(true);
+
+    try {
+      const requestBody = {
+        title: newTitle,
+        text: newText,
+        coverImageURL: newCoverImageURL,
+        users: checkableUsers
+          ? checkableUsers
+              .filter((user) => user.isSelected)
+              .map((user) => user.id)
+          : [],
+        tags: checkableTags
+          ? checkableTags.filter((tag) => tag.isSelected).map((tag) => tag.id)
+          : [],
+      };
+      const requestUrl = `/api/admin/contents/${id}`;
+      console.log(`${requestUrl} => ${JSON.stringify(requestBody, null, 2)}`);
+      const res = await fetch(requestUrl, {
+        method: "PUT",
+        cache: "no-store",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(requestBody),
+      });
+
+      if (!res.ok) {
+        throw new Error(`${res.status}: ${res.statusText}`);
+      }
+
+      setIsSubmitting(false);
+      router.push("/admin/contents");
+    } catch (error) {
+      console.error(`エラーが発生しました: ${error}`);
+      setIsSubmitting(false);
+    }
+  };
+
+  if (fetchError) {
+    return <div>{fetchError}</div>;
+  }
+
+  if (!isInitialized || !checkableTags || !checkableUsers) {
+    return <div>初期化中...</div>;
+  }
+
+  return (
+    <main>
+      <div className="mb-4 text-2xl font-bold">コンテンツの編集・削除</div>
+
+      {isSubmitting && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
+          <div className="flex items-center rounded-lg bg-white px-8 py-4 shadow-lg">
+            <FontAwesomeIcon icon={faSpinner} spin className="text-blue-500" />
+            <div className="ml-4">送信中...</div>
+          </div>
+        </div>
+      )}
+
+      <form
+        onSubmit={handleSubmit}
+        className={twMerge("space-y-4", isSubmitting && "opacity-50")}
+      >
+        <div className="space-y-1">
+          <label htmlFor="title" className="block font-bold">
+            タイトル
+          </label>
+          <input
+            type="text"
+            id="title"
+            name="title"
+            className="w-full rounded-md border-2 px-2 py-1"
+            value={newTitle}
+            onChange={updateNewTitle}
+            placeholder="タイトルを記入してください"
+            required
+          />
+        </div>
+
+        <div className="space-y-1">
+          <label htmlFor="content" className="block font-bold">
+            本文
+          </label>
+          <textarea
+            id="content"
+            name="content"
+            className="h-48 w-full rounded-md border-2 px-2 py-1"
+            value={newText}
+            onChange={updateNewText}
+            placeholder="本文を記入してください"
+            required
+          />
+        </div>
+
+        <div className="space-y-1">
+          <label htmlFor="coverImageURL" className="block font-bold">
+            カバーイメージ (URL)
+          </label>
+          <input
+            type="url"
+            id="coverImageURL"
+            name="coverImageURL"
+            className="w-full rounded-md border-2 px-2 py-1"
+            value={newCoverImageURL}
+            onChange={updateNewCoverImageURL}
+            placeholder="カバーイメージのURLを記入してください"
+            required
+          />
+        </div>
+
+        <div className="space-y-1">
+          <div className="font-bold">ユーザー</div>
+          <div className="flex flex-wrap gap-x-3.5">
+            {checkableUsers.length > 0 ? (
+              checkableUsers.map((chUser) => (
+                <label key={chUser.id} className="flex space-x-1">
+                  <input
+                    type="checkbox"
+                    name="users"
+                    value={chUser.id}
+                    className="rounded-md"
+                    onChange={() => {}}
+                  />
+                  <span className="cursor-pointer">{chUser.name}</span>
+                </label>
+              ))
+            ) : (
+              <div className="">選択可能なユーザーが存在しません</div>
+            )}
+          </div>
+        </div>
+
+        <div className="space-y-1">
+          <div className="font-bold">タグ</div>
+          <div className="flex flex-wrap gap-x-3.5">
+            {tags ? (
+              tags.map((tag) => (
+                <label key={tag.id} className="flex space-x-1">
+                  <input
+                    type="checkbox"
+                    name="tags"
+                    value={tag.id}
+                    className="rounded-md"
+                    onChange={() => {}}
+                  />
+                  <span className="cursor-pointer">{tag.name}</span>
+                </label>
+              ))
+            ) : (
+              <div className="">選択可能なタグが存在しません</div>
+            )}
+          </div>
+        </div>
+
+        <div className="flex justify-end space-x-2">
+          <button
+            type="submit"
+            className={twMerge(
+              "rounded-md px-5 py-1 font-bold",
+              "bg-indigo-500 text-white hover:bg-indigo-600",
+              "disabled:cursor-not-allowed"
+            )}
+            disabled={isSubmitting}
+          >
+            記事を更新
+          </button>
+
+          <button
+            type="button"
+            className={twMerge(
+              "rounded-md px-5 py-1 font-bold",
+              "bg-red-500 text-white hover:bg-red-600"
+            )}
+            // onClick={handleDelete}
+          >
+            削除
+          </button>
+        </div>
+      </form>
+    </main>
+  );
+};
 export default Page;
---
+++ src/app/api/contents/[id]/route.ts
@@ -0,0 +1,60 @@
+import prisma from "@/lib/prisma";
+import { NextResponse, NextRequest } from "next/server";
+
+type RouterParams = {
+  params: {
+    id: string;
+  };
+};
+
+export const GET = async (req: NextRequest, routeParams: RouterParams) => {
+  try {
+    const id = routeParams.params.id;
+
+    const content = await prisma.content.findUnique({
+      where: { id },
+      select: {
+        id: true,
+        title: true,
+        text: true,
+        coverImageURL: true,
+        users: {
+          select: {
+            user: {
+              select: {
+                id: true,
+                name: true,
+                userImageURL: true,
+              },
+            },
+          },
+        },
+        tags: {
+          select: {
+            tag: {
+              select: {
+                id: true,
+                name: true,
+              },
+            },
+          },
+        },
+        createdAt: true,
+      },
+    });
+
+    if (!content) {
+      return NextResponse.json(
+        { error: "コンテンツが見つかりません" },
+        { status: 404 }
+      );
+    }
+    return NextResponse.json(content);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "コンテンツの取得に失敗しました" },
+      { status: 500 }
+    );
+  }
+};
