d8755c92dc4335e27226eb8ae6ccbf8cae450fa1
ai10pro <106215301+ai10pro@users.noreply.github.com>
2025-02-21 16:30:01 +0900

    ユーザー一覧の作成

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -8,10 +8,10 @@ export const GET = async (req: NextRequest) => {
   try {
     const users: User[] = await prisma.user.findMany({
       orderBy: {
-        createdAt: "desc", // 降順 (新しい順)
+        createdAt: "asc",
       },
     });
-    // console.log(users);
+    console.log(users);
     return NextResponse.json(users);
   } catch (error) {
     console.error(error);
